563bc5160f093ae3eaefef8bbfe30e30caf2b1a2
Sundaresan <sundaresanv2004@gmail.com>
2026-02-08 20:42:57 +0530

    added Docker file and docker compose

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // Enables Docker optimization with minimal production build
 };
 
 export default nextConfig;
+
